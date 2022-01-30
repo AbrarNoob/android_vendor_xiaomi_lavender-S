8c13e31cbdfed5a0156b4d14303345f22333f3ae
abrar <hasinabrarhaque2000@gmail.com>
2022-01-30 13:01:49 +0000

    lavender: drop QTI datastatusnotification

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1741,7 +1741,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
